ea6653debac6dc2ccecac576ec9e6fe457ca02b0
Niquinhoo <nicotrib699@gmail.com>
2026-04-23 13:18:00 -0300

    US5

--- routes/checkout.router.js
+++ routes/checkout.router.js
@@ -1,16 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-function getCartItemCount(session) {
-    const cart = Array.isArray(session.cart) ? session.cart : [];
-
-    return cart.reduce((total, item) => total + (Number(item.quantity) || 0), 0);
-}
-
 router.get('/', (req, res) => {
-    res.render('pages/checkout/checkout-page', {
-        cartItemCount: getCartItemCount(req.session)
-    });
+    res.render('pages/checkout/checkout-page');
 });
 
 module.exports = router;
